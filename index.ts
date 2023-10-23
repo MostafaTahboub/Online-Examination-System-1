62dd64a4ede922a9798791e1ac736bad60100f90
mostafatahboub <201129@ppu.edu.ps>
2023-10-23 21:42:12 +0300

    some edit to check push action

--- index.ts
+++ index.ts
@@ -39,7 +39,7 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, async () => {
-  console.log(`App is lestining to PORT  : ` + PORT);
+  console.log(`App is lestining to PORT .. : ` + PORT);
   baseLogger.info(`App is lestining to PORT : ${PORT}`);
 
   dataSource
