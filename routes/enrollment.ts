7b510d0a0f0c28368e73bd9813e3eb7e6695702c
Mohammad-taradeh <98043145+Mohammad-taradeh@users.noreply.github.com>
2023-10-25 19:23:45 +0300

    send email when enroll student

--- routes/enrollment.ts
+++ routes/enrollment.ts
@@ -1,53 +1,72 @@
 import express from "express";
 import { User } from "../DB/Entities/User.js";
-import { validateExamEnrollment, validateUserEnrollment, }
-  from "../middleware/validation/enrollmentValidation.js";
+import {
+  validateExamEnrollment,
+  validateUserEnrollment,
+} from "../middleware/validation/enrollmentValidation.js";
 import { Exam } from "../DB/Entities/Exam.js";
 import { Enrollment } from "../DB/Entities/Enrollment.js";
 import baseLogger from "../log.js";
 import { authenticate } from "../middleware/auth/authenticate.js";
+import sendEmail from "../controllers/SES.js";
 
 var router = express.Router();
 
-router.post("/enroll", validateUserEnrollment, validateExamEnrollment, authenticate, async (req, res) => {
-  try {
-    
-    const userId = req.body.userId;
-    const examId = req.body.examId;
+router.post(
+  "/enroll",
+  validateUserEnrollment,
+  validateExamEnrollment,
+  authenticate,
+  async (req, res) => {
+    try {
+      const userId = req.body.userId;
+      const examId = req.body.examId;
 
-    const previousEnrollment = await Enrollment.find({
-      relations: {
-        user: true,
-        exam: true,
-      },
-      where: {
-        user: {
-          id: userId,
+      const previousEnrollment = await Enrollment.find({
+        relations: {
+          user: true,
+          exam: true,
         },
-        exam: {
-          id: examId,
+        where: {
+          user: {
+            id: userId,
+          },
+          exam: {
+            id: examId,
+          },
         },
-      },
-    });
+      });
 
-    if (!(previousEnrollment.length === 0)) {
-      baseLogger.info(`Trying to enroll user already enrolled in the same exam`);
-      return res.status(403).send("The student already enrolled in this exam ");
+      if (!(previousEnrollment.length === 0)) {
+        baseLogger.info(
+          `Trying to enroll user already enrolled in the same exam`
+        );
+        return res
+          .status(403)
+          .send("The student already enrolled in this exam ");
+      }
+
+      const enrollment = new Enrollment();
+      enrollment.user = userId;
+      enrollment.exam = examId;
+
+      await enrollment.save();
+      const existingUser = await User.findOneBy({ id: userId });
+      const existingExam = await Exam.findOneBy({ id: examId });
+      sendEmail(
+        `${existingUser?.email}`,
+        "Exam Enrollment",
+        `Hi ${existingUser?.name} you have been enrolled to exam with this id: ${existingExam?.id} starts at: ${existingExam?.startTime}. Get Ready`
+      );
+      baseLogger.info(`Enrollment created successfully: ${enrollment.id}`);
+      res
+        .status(201)
+        .json({ msg: "enrollment created successfully", enrollment });
+    } catch (error) {
+      baseLogger.error("Error while creating Enrollment" + error);
+      res.status(500).send("something went Wrong");
     }
-
-    const enrollment = new Enrollment();
-    enrollment.user = userId;
-    enrollment.exam = examId;
-
-    await enrollment.save();
-    baseLogger.info(`Enrollment created successfully: ${enrollment.id}`);
-    res.status(201).json({ msg: "enrollment created successfully", enrollment });
-
-  } catch (error) {
-    baseLogger.error("Error while creating Enrollment" + error);
-    res.status(500).send("something went Wrong");
   }
-}
 );
 
 export default router;
